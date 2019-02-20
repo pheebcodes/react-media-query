db401e06c7072ac72d66f00ccfe1376c1bb04709
Chance Dickson <me@chancedickson.com>
2019-02-20 16:46:59 -0600

    Initial commit

---
+++ rollup.config.js
@@ -0,0 +1,8 @@
+export default {
+  input: 'src/index.js',
+  output: {
+    file: 'cjs/index.js',
+    format: 'cjs'
+  },
+  external: ['react']
+};
---
+++ src/index.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+export function useMediaQuery(query) {
+  const lastQuery = React.useRef();
+  const mq = React.useRef();
+  const callback = React.useRef();
+  const [matches, setMatches] = React.useState();
+
+  if (lastQuery.current !== query) {
+    if (mq.current) {
+      mq.current.removeListener(callback.current);
+      mq.current = null;
+      callback.current = null;
+    }
+
+    if (query) {
+      mq.current = matchMedia(query);
+
+      callback.current = () => {
+        setMatches(mq.current.matches);
+      };
+
+      mq.current.addListener(callback.current);
+
+      if (lastQuery.current === undefined) {
+        setMatches(mq.current.matches);
+      }
+    }
+
+    lastQuery.current = query;
+  }
+
+  return matches;
+}
+
+export function MediaQuery({ query, children }) {
+  const matches = useMediaQuery(query);
+  return matches ? children : null;
+}
