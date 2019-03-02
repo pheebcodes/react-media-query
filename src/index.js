3dae9f1b64416f31c4e6e18eaf3a7e69d382c7b2
Chance Dickson <me@chancedickson.com>
2019-03-02 00:35:13 -0600

    Use useLayoutEffect instead of refs

--- src/index.js
+++ src/index.js
@@ -1,40 +1,25 @@
 import React from "react";
 
 export function useMediaQuery(query) {
-  const lastQuery = React.useRef();
-  const cancel = React.useRef();
   const [matches, setMatches] = React.useState();
 
-  if (lastQuery.current !== query) {
-    if (cancel.current) {
-      cancel.current();
-      cancel.current = null;
-    }
-
-    if (query) {
-      const mq = window.matchMedia(query);
-      const cb = () => {
+  React.useLayoutEffect(
+    () => {
+      if (query) {
+        const mq = window.matchMedia(query);
+        const cb = () => {
+          setMatches(mq.matches);
+        };
+        mq.addListener(cb);
         setMatches(mq.matches);
-      };
-      mq.addListener(cb);
 
-      cancel.current = () => {
-        mq.removeListener(cb);
-      };
-
-      if (
-        lastQuery.current === undefined ||
-        lastQuery.current === null ||
-        lastQuery.current === ""
-      ) {
-        setMatches(mq.matches);
+        return () => {
+          mq.removeListener(cb);
+        };
       }
-    }
-
-    lastQuery.current = query;
-  }
-
-  React.useEffect(() => cancel.current, [cancel.current]);
+    },
+    [query]
+  );
 
   return matches;
 }
